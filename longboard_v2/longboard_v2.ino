5b8bbf145e8cac1434a91c0e8f4fc933394a1347
Floxles <felix.puettner@gmail.com>
2016-05-08 23:09:27 +0200

    test

---
+++ longboard_v2/longboard_v2.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		longboard_v2.ino
+ Created:	08.05.2016 21:33:52
+ Author:	Felix
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+	//test
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
